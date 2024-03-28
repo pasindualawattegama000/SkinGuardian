5d9cca12a64a113f644bc2945d3a4440b8d5366a
pasindualawattegama000 <pasindu.20200732@iit.ac.lk>
2024-03-29 00:37:07 +0530

    Unnecessary file removed

--- test.py
+++
@@ -1,5 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-# Replace 'admin_password_here' with your actual password
-hashed_password = generate_password_hash('thisIsAdmin1')
-print(hashed_password)
